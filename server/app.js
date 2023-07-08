cac8408ff15c737e3d0b6c7b8b606ec570420e6f
Amaan Ansari <ansari028amaan@gmail.com>
2023-07-08 20:08:15 +0530

    fd

--- server/app.js
+++ server/app.js
@@ -31,9 +31,9 @@ app.use("/api/v1/users", userRoute);
 app.use("/api/v1/about", aboutRoute);
 app.post("/contact", sendMail);
 
-app.all("*", (req, res, next) => {
-  next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
-});
+// app.all("*", (req, res, next) => {
+//   next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
+// });
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
